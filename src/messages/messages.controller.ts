84750ddbbd0237193dfbf06e764e3022c3957842
Rahat47 <djrayhan8@gmail.com>
2021-12-21 12:04:20 +0600

    added dependecy injection DI

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -11,12 +11,7 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-    messagesService: MessagesService;
-
-    constructor() {
-        // DONT DO THIS IN Real APPS
-        this.messagesService = new MessagesService();
-    }
+    constructor(public messagesService: MessagesService) {}
 
     @Get()
     listMessages() {
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,20 +1,19 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-    repository: MessagesRepository;
-    constructor() {
-        this.repository = new MessagesRepository();
-    }
+    constructor(public messagesRepository: MessagesRepository) {}
 
     async findOne(id: string) {
-        return this.repository.findOne(id);
+        return this.messagesRepository.findOne(id);
     }
 
     async findAll() {
-        return this.repository.findAll();
+        return this.messagesRepository.findAll();
     }
 
     async create(content: string) {
-        return this.repository.create(content);
+        return this.messagesRepository.create(content);
     }
 }
